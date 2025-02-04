554708db051a6f9a3b319a857b4aeb7578c40498
Murilo <murkuehne@gmail.com>
2025-02-04 11:53:02 -0300

    done

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("footer");
 const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 //Allows the amount input to only receive Numbers
 amount.addEventListener("input", () => {
@@ -35,7 +36,23 @@ form.onsubmit = (event) => {
 // Exchange math function
 function convertCurrency(amount, price, symbol) {
   try {
-    description.textContent = `${symbol} 1 = ${price}`;
+    // Shows the coin exchange value
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    // calculates the total
+    let total = amount * price;
+
+    // alerts if any bug happens
+    if (isNaN(total)) {
+      return alert("Digite apenas números para serem convertidos.");
+    }
+
+    // Formats the total value
+    total = formatCurrencyBRL(total);
+
+    // shows the total
+    result.textContent = total;
+
     // Add the show-result class to the footer
     footer.classList.add("show-result");
   } catch (error) {
@@ -45,4 +62,13 @@ function convertCurrency(amount, price, symbol) {
     console.log(error);
     alert("Não foi possível converter. Tente novamente!");
   }
+}
+
+// Formats the base coin as BRL
+function formatCurrencyBRL(value) {
+  // Convert the value to a Number to use toLocaleString to format as BRL pattern (R$ 00,00)
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
